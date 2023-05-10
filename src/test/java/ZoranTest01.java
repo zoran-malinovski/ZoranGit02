f689c099e87a13873d97a997d5160925a7c678af
Zoran <z.malinovski79@gmail.com>
2023-05-10 20:11:01 +0200

    added branch-02 and make change

--- src/test/java/ZoranTest01.java
+++ src/test/java/ZoranTest01.java
@@ -34,7 +34,7 @@ public class ZoranTest01 {
 
     @Test
     public void ZoranThirdTest(){
-        System.out.println("Kako si Malinovski");
+        System.out.println("Kako si Malinovski. Dobro utro?");
 
         WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "C:\\Chrome Drivers\\chromedriver.exe");
